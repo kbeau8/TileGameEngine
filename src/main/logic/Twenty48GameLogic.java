c5c2febfe69564b48b68c328971f87b5604dac6f
alicetran1221 <108845502+alicetran1221@users.noreply.github.com>
2025-03-13 21:12:33 -0700

    changed 2048 game logic to update the player profile

--- src/main/logic/Twenty48GameLogic.java
+++ src/main/logic/Twenty48GameLogic.java
@@ -1,36 +1,51 @@
 package logic;
+
 import tiles.Twenty48TileMap;
+import profiles.PlayerProfile;
 
 public class Twenty48GameLogic extends GameLogic {
     public void checkWinConditions(Twenty48TileMap tilemap) {
-        if(tilemap.is2048()) {
-            //do win condition
+        if (tilemap.is2048()) {
+            // do win condition
         }
     }
 
     public boolean isGameOver(Twenty48TileMap tilemap) {
-        //no empty space on board AND no tiles to combine
-        if(tilemap.isEmptySpace()) {
+        // no empty space on board AND no tiles to combine
+        if (tilemap.isEmptySpace()) {
             return false;
         }
         return true;
     }
 
+    public void updateScore(int score, PlayerProfile player) {
+        // update player's high score if higher
+        if (score > player.getHighScore("2048")) {
+            player.updateHighScore("2048", score);
+        }
+        // update actual game score
+    }
+
+    // unsure about how to use this method - AT
+    public int getScore(int score) {
+        return score;
+    }
+
+    // UNUSED FUNCTIONS
+    @Override
+    public void checkWinConditions() {
+    }
+
+    public boolean isGameOver() {
+        return false;
+    }
+
     @Override
     public void updateScore() {
-        // Implement the score update logic for the 2048 game
     }
 
     @Override
     public void getScore() {
-        // Implement the score retrieval logic for the 2048 game
-    }
-
-    //UNUSED FUNCTIONS
-    @Override 
-    public void checkWinConditions() {}
-    public boolean isGameOver() {
-        return false;
     }
 
 }
