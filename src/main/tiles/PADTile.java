4826fbb1847730f4be10052297db0b1a5e6faea2
podskio <podskio1@gmail.com>
2025-03-15 17:14:31 -0700

    Background and pixelated tiles, populate board, tile class

--- src/main/tiles/PADTile.java
+++ src/main/tiles/PADTile.java
@@ -1,5 +1,43 @@
 package tiles;
 
+import java.awt.Image;
+import java.util.HashMap;
+
+class PADTileImages {
+    static final HashMap<PADTile.PADTileType, Image> imageMap = new HashMap<>();
+    static final String ASSET_PATH = "pad-assets/";
+
+    static {
+        for (PADTile.PADTileType type : PADTile.PADTileType.values()) {
+            Image image = new javax.swing.ImageIcon(ASSET_PATH + "tile-" + type.toString().toLowerCase() + ".png").getImage();
+            imageMap.put(type, image);
+        }
+    }
+}
+
 public class PADTile extends Tile {
-    public PADTile() {}
+    public enum PADTileType {
+        FIRE, WOOD, WATER, LIGHT, DARK
+    }
+
+    public PADTileType type;
+
+    public PADTile(PADTileType type, int x, int y) {
+        super();
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.image = PADTileImages.imageMap.get(type);
+    }
+
+    public PADTileType getType() {
+        return this.type;
+    }
+
+    public Image getImage() { return this.image; }
+
+    public void setType(PADTileType type) {
+        this.type = type;
+        this.image = PADTileImages.imageMap.get(type);
+    }
 }
--- src/main/tiles/PADTileMap.java
+++ src/main/tiles/PADTileMap.java
@@ -2,17 +2,29 @@ package tiles;
 
 public class PADTileMap extends TileMap {
   public PADTile[][] tiles;
-  public int score;
 
-  public PADTileMap() {
+  public PADTileMap(int height, int width) {
     super();
-    this.height = 5;
-    this.width = 6;
+    this.height = height;
+    this.width = width;
 
     initializeTiles();
   }
 
+  public PADTile.PADTileType getRandomTile() {
+    PADTile.PADTileType[] tiles = PADTile.PADTileType.class.getEnumConstants();
+    return tiles[(int) (Math.random() * tiles.length)];
+  }
+
   private void initializeTiles() {
-    // randomly populate board
+    this.tiles = new PADTile[height][width];
+
+    // Randomly populate the board
+    for (int y = 0; y < height; ++y) {
+      for (int x = 0; x < width; ++x) {
+        System.out.println("New tile " + x + " " + y);
+        this.tiles[y][x] = new PADTile(getRandomTile(), x, y);
+      }
+    }
   }
 }
